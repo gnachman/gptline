25919953ebba5d7df2e87f6e0b575ec7796866d1
George Nachman <gnachman@gmail.com>
2023-09-18 14:28:49 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="gptline",
-    version="1.0.2",
+    version="1.0.3",
     packages=["src"],
     install_requires=requirements,
     entry_points={
